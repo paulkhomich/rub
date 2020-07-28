d2a78d76e714f5a3aec0c7cf53ae009da8821a50
Paul Khomich <paulkhomich@outlook.com>
2020-07-28 22:22:51 +0500

    Initial Commit

---
+++ rub/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rub
+//
+//  Created by Павел Хомич on 28.07.2020.
+//  Copyright © 2020 Павел Хомич. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
